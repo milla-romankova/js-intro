f80eead6d0250f4fdf62c197946453ed9d1ab6b1
milla-romankova <milla.romankova@gmail.com>
2020-06-24 16:16:15 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//alert('Hello JavaScript!');//
+
+let name = 'John';
+alert(name);
+name = 'James';
+alert(name);
+
+const greetings = 'Hello ' + name;
+alert(greetings);\
+
+const userAge = prompt('Enter your age');
+alert('Your age is ' + userAge);
+
+const nextAge = userAge + "1"
+
+
